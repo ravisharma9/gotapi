7215a593b8b5d3915a7787774e32189f3083ae54
RaviJubi <ravi@jubi.ai>
2019-09-14 00:28:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var request = require('request')
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(process.env.PORT || 80);
